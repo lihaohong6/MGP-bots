3c8668f139af8e38df1452354e001a46f3fcc6fd
peter <lih@lafayette.edu>
2022-08-27 04:42:14 -0400

    add vtuber commons tool

--- user-config.py
+++ user-config.py
@@ -1,4 +1,4 @@
-family_files['mgp'] = 'https://mzh.moegirl.org.cn/api.php'
+family_files['mgp'] = 'https://zh.moegirl.org.cn/api.php'
 family_files['commons'] = 'https://commons.moegirl.org.cn/api.php'
 family_files['en'] = 'https://en.moegirl.org.cn/api.php'
 mylang = 'mgp'
@@ -9,7 +9,7 @@ usernames['mgp']['*'] = 'Lihaohong'
 usernames['en']['*'] = 'Lihaohong'
 password_file = "user-password.py"
 
-minthrottle = 3
+minthrottle = 5
 maxthrottle = 20
 
 put_throttle = 20
