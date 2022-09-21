89beb9a06a7983c61566d594720c8170f6b59740
peter <lih@lafayette.edu>
2022-09-21 01:35:42 -0400

    experimental time delay for bot edits

--- bots/recent_changes_bot.py
+++ bots/recent_changes_bot.py
@@ -1,11 +1,13 @@
+import datetime
 from abc import ABC
 
 import pywikibot
-from pywikibot import APISite, Page
+from pywikibot import APISite, Page, Timestamp
 from pywikibot.bot import SingleSiteBot
 from pywikibot.pagegenerators import PreloadingGenerator
 
 from utils.config import get_rate_limit, get_data_path
+from datetime import timezone, datetime, timedelta
 
 
 def filter_recent_changes(resume_id: int, recent_changes_generator):
@@ -27,19 +29,27 @@ class RecentChangesBot(SingleSiteBot, ABC):
     from utils.sites import mgp
 
     def __init__(self, bot_name: str, resume_id: int = None, site: APISite = mgp, group_size: int = get_rate_limit(),
-                 ns: str = "0", **kwargs):
+                 ns: str = "0", time_offset: int = -0.1, **kwargs):
         super(RecentChangesBot, self).__init__(site=site, **kwargs)
         self.group_size = group_size
         self.resume_file = get_data_path().joinpath(bot_name + "_resume.txt")
+        # TODO: create a separate resume file for new pages
         if self.resume_file.exists() and resume_id is None:
             try:
                 with open(self.resume_file, "r") as f:
                     resume_id = int(f.read())
             except Exception as e:
                 pywikibot.error(e)
+        # use current time
+        cur_time = datetime.now(tz=timezone(offset=timedelta(hours=8), name="CST"))
+        # compute recent changes delay based on offset
+        rc_time = cur_time + timedelta(hours=time_offset)
+        time_start = Timestamp(rc_time.year, rc_time.month, rc_time.day,
+                               rc_time.hour, rc_time.minute, rc_time.second,
+                               tzinfo=rc_time.tzinfo)
         self.gen = filter_recent_changes(resume_id,
                                          site.recentchanges(namespaces=ns, bot=False, redirect=False,
-                                                            changetype='edit|new'))
+                                                            changetype='edit|new', start=time_start, top_only=True))
         self._start_ts = pywikibot.Timestamp.now()
 
     def run(self) -> None:
