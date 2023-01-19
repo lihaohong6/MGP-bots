b0e7b8ebc0706700c18b2dba3dd25ae98355c379
peter <lih@lafayette.edu>
2023-01-19 03:25:06 -0500

    refactor template splitter

--- bots/template_splitter.py
+++ bots/template_splitter.py
@@ -1,6 +1,7 @@
 import re
 import sys
 from argparse import ArgumentParser
+from typing import Dict, List, Set
 
 import wikitextparser as wtp
 from pywikibot import Page
@@ -24,7 +25,7 @@ def template_splitter(name, aliases, limit: int = 50):
                  if t.name.lower() == 'navbox' and
                  t.get_arg("1") is not None and
                  t.get_arg("1").value.strip() == 'child']
-    pages_dict = dict()
+    pages_dict: Dict[str, Page] = dict()
     changed_pages = set()
     for nav in nav_boxes:
         nav_section_name = re.search(r"{#switch:([^|]+)\|", str(nav))
@@ -36,33 +37,11 @@ def template_splitter(name, aliases, limit: int = 50):
         nav_pages = [Page(source=site, title=link.title)
                      for link in nav.wikilinks]
         gen = PreloadingGenerator(nav_pages)
-        for page in gen:
-            # ignore pages that don't exist
-            if not page.exists():
-                continue
-            if '#' == page.text[0] and page.isRedirectPage():
-                page = page.getRedirectTarget()
-            page_title = page.title(allow_interwiki=False)
-            if page_title in pages_dict:
-                page = pages_dict[page_title]
-            else:
-                pages_dict[page_title] = page
-            parsed = wtp.parse(page.text)
-            target_template = find_templates(parsed.templates, *template_names)
-            if len(target_template) != 1:
-                print("Page " + page.title() +
-                      " has no template with the desired name."
-                      if len(target_template) == 0
-                      else " has multiple templates with the desired name.")
-                continue
-            target_template = target_template[0]
-            for arg in target_template.arguments:
-                if arg.positional and arg.value == nav_section_name:
-                    break
-            else:
-                target_template.set_arg("", nav_section_name, positional=True)
-                setattr(page, "_text", str(parsed))
-                changed_pages.add(page_title)
+        process_navbox_pages(changed_pages, gen, nav_section_name, pages_dict, template_names)
+    save_changed_pages(changed_pages, limit, pages_dict)
+
+
+def save_changed_pages(changed_pages, limit, pages_dict):
     counter = 0
     for page_title, page in pages_dict.items():
         if limit != -1 and counter >= limit:
@@ -71,6 +50,37 @@ def template_splitter(name, aliases, limit: int = 50):
         if page_title in changed_pages:
             page.save(summary="批量添加大家族模板参数", force=True, **get_default_save_params())
             counter += 1
+
+
+def process_navbox_pages(changed_pages: Set[str], gen, nav_section_name: str, pages_dict: Dict[str, Page],
+                         template_names: Set[str]):
+    for page in gen:
+        # ignore pages that don't exist
+        if not page.exists():
+            continue
+        if '#' == page.text[0] and page.isRedirectPage():
+            page = page.getRedirectTarget()
+        page_title = page.title(allow_interwiki=False)
+        if page_title in pages_dict:
+            page = pages_dict[page_title]
+        else:
+            pages_dict[page_title] = page
+        parsed = wtp.parse(page.text)
+        target_template = find_templates(parsed.templates, *template_names)
+        if len(target_template) != 1:
+            print("Page " + page.title() +
+                  " has no template with the desired name."
+                  if len(target_template) == 0
+                  else " has multiple templates with the desired name.")
+            continue
+        target_template = target_template[0]
+        for arg in target_template.arguments:
+            if arg.positional and arg.value == nav_section_name:
+                break
+        else:
+            target_template.set_arg("", nav_section_name, positional=True)
+            setattr(page, "_text", str(parsed))
+            changed_pages.add(page_title)
 
 
 def run_template_splitter():
