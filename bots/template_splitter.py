b5f37e672defbe4720d11a920bd5f9f490bbe4a7
peter <lih@lafayette.edu>
2023-03-05 20:37:45 -0500

    add comments to template splitter

--- bots/template_splitter.py
+++ bots/template_splitter.py
@@ -16,10 +16,14 @@ site = mgp()
 
 def template_splitter(name, aliases, limit: int = 50):
     template_page = Page(source=site, title="Template:" + name)
+    # 获取重定向
     template_names = set(p.title(with_ns=False, allow_interwiki=False)
                          for p in template_page.redirects(namespaces="Template"))
+    # 添加模板名本身
     template_names.add(name)
+    # 添加alias中其它不是重定向的模板名（如简繁转换）
     template_names.update(set(aliases))
+
     parsed = wtp.parse(template_page.text)
     # TODO: how about #invoke:nav|box|child and {{Navbox with collapsible groups}}
     nav_boxes = [t for t in parsed.templates
@@ -30,6 +34,7 @@ def template_splitter(name, aliases, limit: int = 50):
     changed_pages = set()
     for nav in nav_boxes:
         # for manual navboxes that depend on a switch
+        # 获取当前部分的名字
         nav_section_name = re.search(r"{#switch:([^|]+)\|", str(nav))
         if nav_section_name is not None:
             nav_section_name = nav_section_name.group(1)
@@ -39,8 +44,10 @@ def template_splitter(name, aliases, limit: int = 50):
             continue
 
         print("Processing " + nav_section_name)
+        # 获取当前部分的所有内部链接以便处理它们
         nav_pages = [Page(source=site, title=link.title)
                      for link in nav.wikilinks]
+        # 预加载这些页面
         gen = PreloadingGenerator(nav_pages)
         process_navbox_pages(changed_pages, gen, nav_section_name, pages_dict, template_names)
     save_changed_pages(changed_pages, limit, pages_dict)
@@ -60,17 +67,20 @@ def save_changed_pages(changed_pages, limit, pages_dict):
 def process_navbox_pages(changed_pages: Set[str], gen, nav_section_name: str, pages_dict: Dict[str, Page],
                          template_names: Set[str]):
     for page in gen:
-        # ignore pages that don't exist
+        # 忽略不存在的页面
         if not page.exists():
             continue
+        # 如果是重定向，获取重定向目标
         if '#' == page.text[0] and page.isRedirectPage():
             page = page.getRedirectTarget()
         page_title = page.title(allow_interwiki=False)
+        # 如果之前处理过这个页面，那么直接获取之前的Page，否则使用这次新建的Page
         if page_title in pages_dict:
             page = pages_dict[page_title]
         else:
             pages_dict[page_title] = page
         parsed = wtp.parse(page.text)
+        # 查找目标模板
         target_template = find_templates(parsed.templates, *template_names)
         if len(target_template) != 1:
             print("Page " + page.title() +
@@ -78,13 +88,19 @@ def process_navbox_pages(changed_pages: Set[str], gen, nav_section_name: str, pa
                   if len(target_template) == 0
                   else " has multiple templates with the desired name.")
             continue
+        # 有且只有一个目标模板，继续处理
         target_template = target_template[0]
         for arg in target_template.arguments:
             if arg.positional and arg.value == nav_section_name:
+                # 已有目标参数，跳过
                 break
         else:
+            # 添加匿名参数，内容为这部分的标题
             target_template.set_arg("", nav_section_name, positional=True)
+            # 把parsed转回str并保存到页面
+            # 直接用setter会多一个检查_bot_may_edit的步骤，这样可以节省时间
             setattr(page, "_text", str(parsed))
+            # 本页已被更改
             changed_pages.add(page_title)
 
 
@@ -99,4 +115,5 @@ def run_template_splitter():
         limit = -1
     global site
     site = get_site_by_name(args.site)
+    # 处理模板，多余的模板名被视为该模板的其它名称
     template_splitter(args.template_names[0], args.template_names[1:], limit)
