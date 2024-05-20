2a7c8153da6b3fc89120ea6292734679d97e6b1d
Peter Li <12545295+syccxcc@users.noreply.github.com>
2024-05-20 18:49:09 -0400

    update user-config

--- user-config.py
+++ user-config.py
@@ -4,6 +4,8 @@ family_files['mgp'] = 'https://mzh.moegirl.org.cn/api.php'
 family_files['commons'] = 'https://commons.moegirl.org.cn/api.php'
 family_files['en'] = 'https://en.moegirl.org.cn/api.php'
 family_files['mirror'] = 'https://moegirl.uk/api.php'
+family_files['icu'] = 'https://moegirl.icu/api.php'
+family_files['icu_cm'] = 'https://commons.moegirl.icu/api.php'
 mylang = 'mgp'
 family = 'mgp'
 
@@ -18,3 +20,5 @@ maxthrottle = 20
 put_throttle = 20
 
 noisysleep = 1
+
+user_agent_format = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
